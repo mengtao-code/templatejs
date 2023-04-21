e5bbc6b00971c765317bf3651e6266c653cd1147
mengtao <mengtao@onerzone.com>
2023-04-21 14:32:47 +0800

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    trailingComma: 'es5',
-    semi: false,
-    singleQuote: true,
-    tabWidth: 4,
-    quoteProps: 'as-needed',
-    jsxSingleQuote: true,
-    bracketSpacing: false,
-    parser: 'typescript',
-    printWidth: 160,
-}
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 4,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  bracketSpacing: false,
+  parser: 'typescript',
+  printWidth: 160,
+};
