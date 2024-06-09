283b552a0d2e89fa10ced95913f938799770b343
mengtao <mengtaoxin@outlook.com>
2024-06-10 01:38:40 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import App from 'App'
-import {createElement} from 'react'
-import {createRoot} from 'react-dom/client'
-import '@fontsource/roboto'
-import 'style.css'
+// import App from 'App'
+// import {createElement} from 'react'
+// import {createRoot} from 'react-dom/client'
+// import '@fontsource/roboto'
+// import 'style.css'
 
-const app = document.getElementById('app')
-if (app) {
-    const root = createRoot(app)
-    root.render(createElement(App))
-}
+// const app = document.getElementById('app')
+// if (app) {
+//     const root = createRoot(app)
+//     root.render(createElement(App))
+// }
+console.log('hello world')
